d7c5f499493ee3f714e7a66f4b77db1c063e59f3
Jan Krag <jankrag@gmail.com>
2014-02-28 19:38:38 +0100

    initialized Angular project with Yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pedigraphFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
